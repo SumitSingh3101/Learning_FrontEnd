6daa05df8ae482a2f297860b706f0396c63a8f42
SumitSingh3101 <sumitsingh310106@gmail.com>
2023-11-15 23:30:17 +0530

    Day_7

---
+++ Day 7/PosNeg0.js
@@ -0,0 +1,13 @@
+const n = parseInt(prompt("Enter a number: "));
+
+if (n > 0) {
+    console.log("The number is positive");
+}
+
+else if (n == 0) {
+  console.log("The number is zero");
+}
+
+else {
+     console.log("The number is negative");
+}
