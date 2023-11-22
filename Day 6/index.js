05a2021abc4b1feddcc6dfe76f325c4b47d3aa0e
SumitSingh3101 <sumitsingh310106@gmail.com>
2023-11-22 23:18:59 +0530

    function$

--- Day 6/index.js
+++ Day 6/index.js
@@ -3,4 +3,4 @@ console.log("Hello!")
 let pencilPrice = 10;
 let erasorPrice = 5;
 
-console.log("The total price is :${pencilPrice + erasorPrice} Rupees.");
+console.log(` The total price is :${pencilPrice + erasorPrice} Rupees.`);
