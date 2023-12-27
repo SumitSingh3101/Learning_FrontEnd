8a5f4c71d48f972fad21ca32847d224096b648df
SumitSingh3101 <sumitsingh310106@gmail.com>
2023-12-27 23:28:03 +0530

    Javascript practicing

---
+++ prac.js
@@ -0,0 +1,30 @@
+let student = {
+    name: "sumit",
+    marks: "not taht gud"
+    age: "doesn't matter"
+    sex: "Never"
+}
+
+console.log(post.name)
+console.log(post["sex"])
+
+
+const obj = {
+    1:"a",
+    2:"b",
+    3:"c",
+    true:"d"
+    null:"e"
+}
+
+console.log(obj[1])//here 1 is just a string, it doesn't take index, it just convert the given value in string and then pass//
+console.log(obj.1)//here 1 is not getting converted to string//
+
+
+// How to Add or Update the values
+
+student.city = "Kota"
+
+student.sex = "Male"
+
+console.log(student)
