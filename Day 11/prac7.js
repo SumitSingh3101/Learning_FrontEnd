98fc8b6c0d4d9e15c7491ae37f60f0fa61a9b3d4
SumitSingh3101 <sumitsingh310106@gmail.com>
2024-01-03 23:48:34 +0530

    solving  the question

---
+++ Day 11/prac7.js
@@ -0,0 +1,14 @@
+let str = "abcdancdefgggh"
+
+function getUnique(str){
+    let ans="";
+    for (let i = 0; i < str.length; i++){
+        let currChar = str[i];
+        if (ans.indexOf(currChar) == -1){
+            ans += currChar;
+        }
+    }
+    return ans;
+}
+
+getUnique(str);
