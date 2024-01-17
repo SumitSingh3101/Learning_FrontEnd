767ff07df956239acd1c9871618e9d5b33d10bcd
SumitSingh3101 <sumitsingh310106@gmail.com>
2024-01-17 19:03:00 +0530

    Some finale touchs

--- DOM Starter/app.js
+++ DOM Starter/app.js
@@ -1,18 +1,19 @@
 let btn = document.querySelector("button");
 
 btn.addEventListener("click", function(){
-    let h3 = document.querySelector("h3");
+    let h3 = document.querySelector("#name");
     let randomColor = getRandomColor();
     h3.innerText = randomColor;
 
-    let div = document.querySelector("div");
+    let div = document.querySelector("#color");
     div.style.backgroundColor = randomColor;
+    console.log(randomColor, "Colour updated");
 });
 
 function getRandomColor(){
-    let red = Math.floor{Math.random() + };
-    let green = Math.floor{Math.random() + };
-    let blue = Math.floor{Math.random() + };
+    let red = Math.floor(Math.random() * 225 );
+    let green = Math.floor(Math.random() * 225);
+    let blue = Math.floor(Math.random() * 225);
 
     let color = `rgb(${red}, ${green}, ${blue})`;
     return color;
